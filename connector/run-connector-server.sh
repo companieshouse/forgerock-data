175844716a79728e13719c1e62076952806a8d43
Saul France <saul.france@amido.com>
2021-03-08 15:14:16 +0000

    chore: script for configuring and running connector

---
+++ connector/run-connector-server.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp conf/ConnectorServer.properties.sample conf/ConnectorServer.properties
+sed -i "s|{RCS_CLIENT_SECRET}|${RCS_CLIENT_SECRET}|g" conf/ConnectorServer.properties
+sed -i "s|{FIDC_URL}|$(echo "${FIDC_URL#https://}")|g" conf/ConnectorServer.properties
+sed -i "s|{SERVER_KEY}|${SERVER_KEY}|g" conf/ConnectorServer.properties
+sed -i "s|{CONNECTOR_NAME}|${CONNECTOR_NAME}|g" conf/ConnectorServer.properties
+
+bin/ConnectorServer.sh /setkey ${SERVER_KEY}
+bin/ConnectorServer.sh jpda /run
